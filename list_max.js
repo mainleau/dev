141cecfc416e61cd2d447f4b9662be3d373afd68
Lo <45238635+mainleau@users.noreply.github.com>
2023-03-12 01:32:50 +0100

    Update list_max.js

--- list_max.js
+++ list_max.js
@@ -2,6 +2,6 @@ list_max = ([a, ...b]) => {
   return !b.length ? a : a > list_max(b) ? a : list_max(b);
 }
 
-list_max = ([ a, ...b ], c = a) => {
+list_max = ([a, ...b], c = a) => {
   return !a ? c : list_max(b, a > c ? a : c);
 }
